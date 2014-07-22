507ba4163e9228a52b25ed7e88c758de632789f2
Guy Halperin & Jeff Stringer <student@epicodus.com>
2014-07-22 11:52:27 -0700

    added spec and code for two pairs

--- lib/poker_hand.rb
+++ lib/poker_hand.rb
@@ -1,4 +1,18 @@
-describer('poker_hand') do
-  it("returns 'straight' for a poker hand with 5 consecutively numbered cards")
-
+def poker_hand(hand)
+  hand.sort!
+  counter = 0
+  amount = 0
+  show = ""
+  0.upto(hand.length-2) do |index|
+    if hand[index][0] == hand[index+1][0]
+      show = "one pair"
+      amount += 1
+    end
+    if amount ==2
+      show = "two pair"
+    end
+  end
+  show
 end
+
+poker_hand(['2S', '2C', '4D', '4C', '6H'])
--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -1,1 +1,16 @@
+require ('rspec')
+require ('poker_hand')
 
+describe('poker_hand') do
+  it("returns 'one pair' for a poker hand with 1 pairs of numbered cards") do
+    expect(poker_hand(['2S', '1C', '4D', '4C', '6H'])).to eq("one pair")
+  end
+
+  it("returns 'two pair' for a hand with 2 pairs of numbered cards") do
+    expect(poker_hand(['2S', '2C', '4D', '4C', '6H'])).to eq("two pair")
+  end
+
+  it("returns 'three of a kind' for a hand with 2 pairs of numbered cards") do
+    expect(poker_hand(['2S', '2C', '4D', '4C', '4H'])).to eq("three of a kind")
+  end
+end
